902ed70da73cc23a5ee16f56810b46e494624ba0
Maiury Garcia <maiurygarcia@gmail.com>
2025-07-24 23:18:54 -0300

    feat: Create model Task and adjusts in category model

--- tasks/models.py
+++ tasks/models.py
@@ -4,15 +4,40 @@ from common.models import TimeStampedModel
 
 
 class Category(TimeStampedModel):
-    name = models.CharField(max_length=100, unique=True, blank=False, db_index=True, null=False, verbose_name='Category Name')
-    icon = models.CharField(max_length=100, blank=True, null=True, verbose_name='Icon')
+    name = models.CharField(max_length=100, unique=True, blank=False, db_index=True, null=False, verbose_name='Nome')
+    icon = models.CharField(max_length=100, blank=True, null=True, verbose_name='Ícone')
 
     class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
         ordering = ['name']
 
     def __str__(self):
         return self.name
 
+
+class Task(TimeStampedModel):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, db_index=True, related_name='tasks', verbose_name='Usuário')
+    title = models.CharField(max_length=200, blank=False, null=False, verbose_name='Título')
+    description = models.TextField(blank=True, null=True, verbose_name='Descrição')
+    categories = models.ManyToManyField(Category, related_name='tasks', blank=True, verbose_name='Categorias')
+    status = models.CharField(
+        max_length=50,
+        choices=(
+            ('pending', 'Pendente'),
+            ('in_progress', 'Em andamento'),
+            ('completed', 'Concluída'),
+        ),
+        db_index=True,
+        default='in_progress',
+    )
+    execute_at = models.DateTimeField(db_index=True, verbose_name='Data de execução')
+
+    class Meta:
+        verbose_name = 'Tarefa'
+        verbose_name_plural = 'Tarefas'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
 
