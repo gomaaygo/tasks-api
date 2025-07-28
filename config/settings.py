f714d4def6f4aabd864487937fb385118e2798dc
Maiury Garcia <maiurygarcia@gmail.com>
2025-07-28 08:52:35 -0300

    config: Config redis

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,16 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
