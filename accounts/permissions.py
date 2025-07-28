86f6128f9c9bf97af3bf4664fcddc5632a2683d8
Maiury Garcia <maiurygarcia@gmail.com>
2025-07-28 07:47:08 -0300

    fix: Adjust condition

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -2,7 +2,12 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
+
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.id == request.user.id
+        return obj.user == request.user
