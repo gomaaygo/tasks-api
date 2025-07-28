3d3bcf3e37ea4e62476596e8840c795b89f47256
Maiury Garcia <maiurygarcia@gmail.com>
2025-07-28 12:08:22 -0300

    feat: Rename verbose name model user

--- accounts/models.py
+++ accounts/models.py
@@ -6,13 +6,13 @@ from common.models import TimeStampedModel
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
-    name = models.CharField(max_length=150, verbose_name='Name')
+    name = models.CharField(max_length=150, verbose_name='Nome')
     username = models.CharField(max_length=150, unique=True, db_index=True, verbose_name='Username')
     email = models.EmailField(max_length=255, unique=True, db_index=True, error_messages={'unique': 'User with existing email.'}
                               , verbose_name='Email')
     is_active = models.BooleanField(default=True,
         help_text=('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'),
-        verbose_name='Active?')
+        verbose_name='Ativo?')
     is_staff = models.BooleanField(default=False,
         help_text=('Designates whether the user can log into this admin site.'),
         verbose_name='Acesso ao Dashboard?')
@@ -20,8 +20,8 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
     objects = UserManager()
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
         ordering = ['-created_at']
 
     USERNAME_FIELD = 'email'
